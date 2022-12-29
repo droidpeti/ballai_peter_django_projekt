b69a10d2e3b71f2f9d31b0bcae09e7f2b9792160
droidpeti <82815028+droidpeti@users.noreply.github.com>
2022-12-29 17:37:50 +0100

    Add files via upload
    Első Commit

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    rendelo = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    pizza = models.CharField(max_length=255, default="Marghareita")
+    italok = models.TextField( null=True, max_length=255, default="Coca Cola")
+    leadott_datum = models.DateTimeField(
+            default=timezone.now)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
---
+++ blog/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.shortcuts import redirect
+
+
+def post_list(request):
+    posts = Post.objects.filter(leadott_datum__lte=timezone.now())
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.rendelo = request.user
+            post.leadott_datum = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.rendelo = request.user
+            post.ladott_datum = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
